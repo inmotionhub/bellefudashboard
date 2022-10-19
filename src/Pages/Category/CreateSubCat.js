c230cdcfe07b518a1eab8395aa8881c2a3adde59
felistus <felistusobieze@gmail.com>
2022-10-19 15:50:52 +0200

    feat: made createSubCat responsive on all device

--- src/Pages/Category/CreateSubCat.js
+++ src/Pages/Category/CreateSubCat.js
@@ -2,7 +2,7 @@ import React from "react";
 import { PageTitle } from "../../Constant";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import TextField from "@mui/material/TextField";
 import InputLabel from "@mui/material/InputLabel";
@@ -12,41 +12,30 @@ import Select from "@mui/material/Select";
 import * as Icons from "@mui/icons-material";
 import { colors, APIDATA } from "../../Constant";
 
-import {
-
-  Button,
-  Paper,
-  Box,
-  Toolbar,
-} from "@mui/material";
-
+import { Button, Paper, Box, Toolbar } from "@mui/material";
+import { width } from "@mui/system";
 
 export default function CreateSubCat() {
   const [title, setTitle] = useState(undefined);
   const [selectedFile, setSelectedFile] = useState();
   const [preview, setPreview] = useState(undefined);
   const [catId, setCatId] = useState(null);
-  const [cat, setCat] = useState(null)
+  const [cat, setCat] = useState(null);
 
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
-
     const getCategory = async () => {
-      await axios.get(`${APIDATA}list/category`)
-        .then(res => setCat(res.data.data))
-        .catch(err => console.log(err))
-    }
+      await axios
+        .get(`${APIDATA}list/category`)
+        .then((res) => setCat(res.data.data))
+        .catch((err) => console.log(err));
+    };
 
+    axios.get();
 
-    axios.get()
-
-
-    getCategory()
-  }, [])
-
-
+    getCategory();
+  }, []);
 
   useEffect(() => {
     if (!selectedFile) {
@@ -76,79 +65,92 @@ export default function CreateSubCat() {
     // }
   };
 
-
-
-  const onSubmit = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
     if (title === undefined || catId === null) {
-
-      toast.error('All fields are required', {
-        position: 'top-right'
-      })
+      toast.error("All fields are required", {
+        position: "top-right",
+      });
     } else {
+      const formDatas = new FormData();
 
-      const formDatas = new FormData()
-
-      formDatas.append('name', title)
-      formDatas.append('catid', catId)
+      formDatas.append("name", title);
+      formDatas.append("catid", catId);
 
       axios({
-        method: 'POST',
+        method: "POST",
         url: `${APIDATA}create/sub-category`,
         data: formDatas,
         headers: {
           "Content-Type": "multipart/form-data",
-        }
+        },
       })
-        .then(res => console.log(res))
-        .catch(err => console.log(err))
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
 
-      navigate('/subcatlist')
+      navigate("/subcatlist");
 
-      toast.success('Sub-Category Created', {
-        position: 'top-right'
-      })
-
+      toast.success("Sub-Category Created", {
+        position: "top-right",
+      });
     }
-  }
-
-  const style = {
-    position: "relative",
-    top: 220,
-    left: "45%",
-    transform: "translate(-50%, -50%)",
-    width: 850,
-    bgcolor: "background.paper",
-
-    borderRadius: 5,
-    p: 4,
   };
 
-  const btn = {
-    border: 'none',
-    position: "relative",
-    top: -30,
+  // const style = {
+  //   position: "relative",
+  //   top: 220,
+  //   left: "45%",
+  //   transform: "translate(-50%, -50%)",
+  //   width: 850,
+  //   bgcolor: "background.paper",
 
-    borderRadius: '5px',
-  }
+  //   borderRadius: 5,
+  //   p: 4,
+  // };
+
+  const btn = {
+    border: "none",
+    borderRadius: "5px",
+  };
 
   return (
-    <div style={{ overflow: 'hidden' }}>
+    <Box
+      sx={{
+        height: "calc(100vh - 64px)",
+        width: "100%",
+        overflowX: "hidden",
+      }}
+    >
       <PageTitle title="Create Category" />
-      <div>
-        {/* <Paper sx={{ width: 300, padding: "20px", margin: "auto 4%" }}> */}
-        <Toolbar />
-        <Box>
-          <Paper sx={style}>
-            <form>
-              <div style={{ marginBottom: "5vh", display: "flex" }}>
+      <Box
+        sx={{
+          height: "calc(100vh - 105.98px)",
+          display: { xs: "block", lg: "flex" },
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          maxWidth: 1300,
+          width: "100%",
+        }}
+      >
+        <Paper
+          sx={{
+            width: "100%",
+            maxWidth: 850,
+            height: { xs: "100vh", lg: 450 },
+            p: { xs: 1, md: 5 },
+          }}
+        >
+          <form onSubmit={(e) => onSubmit(e)}>
+            <Box sx={{ display: { xs: "block", md: "flex" } }}>
+              <Box style={{ width: "100%" }}>
                 <div>
-                  <FormControl>
+                  <FormControl sx={{ width: "100%", maxWidth: 350 }}>
                     <InputLabel id="demo-simple-select-label">
                       Select Category
                     </InputLabel>
                     <Select
                       labelId="demo-simple-select-label"
-                      sx={{ mr: 5, width: 250 }}
                       id="demo-simple-select"
                       error={catId === ""}
                       helperText={catId === "" ? "Empty field!" : " "}
@@ -156,17 +158,23 @@ export default function CreateSubCat() {
                       label="Select Category"
                       onChange={(e) => setCatId(e.target.value)}
                     >
-                      {cat?.map(cat => <MenuItem key={cat.name} value={cat.id}>{cat.name}</MenuItem>)}
-
+                      {cat?.map((cat) => (
+                        <MenuItem key={cat.name} value={cat.id}>
+                          {cat.name}
+                        </MenuItem>
+                      ))}
                     </Select>
                   </FormControl>
                 </div>
                 <div>
                   <TextField
-                    sx={{ mr: 5, width: 250 }}
+                    sx={{
+                      width: "100%",
+                      maxWidth: 350,
+                      my: 2,
+                    }}
                     id="outlined-basic"
                     label="Enter Sub-cat Name"
-                    // borderColor='green'
                     type="text"
                     onChange={(e) => setTitle(e.target.value)}
                     value={title}
@@ -180,70 +188,70 @@ export default function CreateSubCat() {
                     multiple
                     type="file"
                     onChange={imageHandler}
-                    style={{ display: 'none' }}
+                    style={{ display: "none" }}
                   />
-
 
                   <Button
                     variant="contained"
                     component="span"
                     startIcon={<Icons.CloudUpload color="#fff" />}
                     sx={{
-                      "&:hover": { backgroundColor: 'green' },
-                      width: 200,
-                      height: 54,
-                      mr: 1,
-                      mt: 3.6,
-
-
-                      bgcolor: '#76ba1b',
-                      color: '#ffff',
-                      // '&:hover': {
-                      //   bgcolor: 'success',
-                      // },
+                      "&:hover": { backgroundColor: "green" },
+                      bgcolor: "#76ba1b",
+                      color: "#ffff",
+                      py: 1,
                     }}
                     style={btn}
                   >
-                    Select  Image
+                    Select Image
                   </Button>
                 </label>
-              </div>
-              <Box>
-                <div style={{ position: "relative", left: "19vw" }}>
-                  {preview === undefined || "" ? (
-                    <Icons.Image style={{ fontSize: "13vw" }} />
-                  ) : (
-                    <img
-                      src={preview}
-                      alt="ajebuta"
-                      style={{
-                        width: 200,
-                        position: "relative",
-                        left: "4vw",
-                      }}
-                    />
-                  )}
-                </div>
               </Box>
-            </form>
-            <Button
-              variant="contained"
-              style={{
-                position: "relative",
-                left: "15.5vw",
-                margin: 10,
-                width: "20vw",
-                backgroundColor: colors.bellefuGreen,
-              }}
-              onClick={onSubmit}
-            >
-              Submit
-            </Button>
-          </Paper>
-        </Box>
+
+              <Box>
+                <Box sx={{ mt: { xs: 2, md: 0 }, mb: 1 }}>
+                  <div
+                    style={{
+                      width: 200,
+                      height: 250,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      borderRadius: 5,
+                      overflow: "hidden",
+                    }}
+                  >
+                    {preview === undefined || "" ? (
+                      <Icons.Image style={{ fontSize: "10vw" }} />
+                    ) : (
+                      <img
+                        src={preview}
+                        alt="uploaded image preview"
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                        }}
+                      />
+                    )}
+                  </div>
+                </Box>
+
+                <Button
+                  variant="contained"
+                  style={{
+                    backgroundColor: colors.bellefuGreen,
+                  }}
+                  onClick={(e) => onSubmit(e)}
+                >
+                  Submit
+                </Button>
+              </Box>
+            </Box>
+          </form>
+        </Paper>
         <Toolbar sx={{ mt: -4 }} />
         {/* </Paper> */}
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 }
